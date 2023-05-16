5bee6c903799af0ea75751f178857e1c9154bcce
Letícia Palaro <109923860+leticiapalaro@users.noreply.github.com>
2023-05-16 19:45:58 -0400

    Update
    ajuste para telas de celular

--- src/components/TelaVitoria/TelaVitoria.js
+++ src/components/TelaVitoria/TelaVitoria.js
@@ -7,13 +7,19 @@ const Formulario = styled(FormStyles)`
   color: rgb(25, 118, 210);
   font-weight: 900;
   text-transform: uppercase;
+
+  @media only screen and (max-width: 768px) {
+    .pressione-enter {
+      display: none;
+    }
+  }
 `
 
 export const TelaVitoria = (props) => {
   return(
     <Formulario>
       <form className="formulario">
-        <p>Pressione <span className="destaque-vermelho">ENTER</span><br />para tentar novamente</p>
+        <p className="pressione-enter">Pressione <span className="destaque-vermelho">ENTER</span><br />para tentar novamente</p>
         {props.resultado}<br />
         <input type='hidden' />
         <BotaoSubmit descricao={'Tentar Novamente'} onClick={props.onClick} />
