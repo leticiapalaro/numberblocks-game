f33325e1e6fea3b0b1793ddf410809aa1a119902
Letícia Palaro <109923860+leticiapalaro@users.noreply.github.com>
2023-05-18 12:17:23 -0400

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,13 +5,16 @@ const HomeContainer = styled.div`
   background: url('./img/background2.webp');
   background-size: cover;
   box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+  text-align: center;
   height: 70vh;
   margin: 5vh 10vw;
   padding: 5vh 5vw;
   width: 80vw;
 
   .img-logo {
-      width: 70vw;
+      width: 550px;
+      max-width: 70vw;
+      margin: auto;
     }
 
   @media screen and (min-width: 1024px) {
@@ -20,7 +23,8 @@ const HomeContainer = styled.div`
     margin: auto;
 
     .img-logo {
-      width: 30vw;
+      max-width: 40vw;
+      margin: auto;
     }
   }
 `
